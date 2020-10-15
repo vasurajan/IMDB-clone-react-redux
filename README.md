f675d7e6485d7733e1bf7d71b32c70a2fe816afb
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-10-16 02:53:39 +0530

    readme file updated

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+<h3>This app is a light clone of IMDB Movie app using React and Redux using external API of TMDB</h3>
+
+
+<h2>Video Walkthrough<h2>
+
 
 ![ezgif com-gif-maker](https://user-images.githubusercontent.com/63305945/95097632-554d4100-074b-11eb-8264-d9922ae5253b.gif)
 
---
+++ build/precache-manifest.ac79054a234678e9dab76ab192307b9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e746d555dc34d5435d2e",
+    "url": "/static/css/main.198cdd28.chunk.css"
+  },
+  {
+    "revision": "e746d555dc34d5435d2e",
+    "url": "/static/js/main.e746d555.chunk.js"
+  },
+  {
+    "revision": "deb97dd84485d644578f",
+    "url": "/static/js/1.deb97dd8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02e9ada22f3fb0de8e2ca571703ea117",
+    "url": "/index.html"
+  }
+];
